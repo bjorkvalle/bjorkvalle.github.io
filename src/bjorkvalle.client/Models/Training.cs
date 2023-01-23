02bce177158df783022dc19c432b7623271e7bb9
Eric Björkvall <eric.bjorkvall@outlook.com>
2023-01-23 22:16:11 +0100

    infrastructure data

---
+++ src/bjorkvalle.client/Models/Training.cs
@@ -0,0 +1,28 @@
+﻿namespace bjorkvalle.client.Models
+{
+    public class TrainingWeek
+    {
+        public string Title { get; set; }
+        public DateTime EndsOn { get; set; }
+        public Day[] Days { get; set; }
+    }
+
+    public class Day
+    {
+        public string Title { get; set; }
+        public Block Block { get; set; }
+    }
+
+    public class Block
+    {
+        public string Title { get; set; }
+        public Exercise[] Exercises { get; set; }
+    }
+
+    public class Exercise
+    {
+        public string Title { get; set; }
+        public string ImgUrl { get; set; }
+    }
+
+}
---
+++ src/bjorkvalle.infrastructure/Usings.cs
@@ -0,0 +1,13 @@
+﻿global using bjorkvalle.infrastructureInfrastructure.Builders;
+global using bjorkvalle.infrastructureInfrastructure.Extensions;
+global using bjorkvalle.infrastructureInfrastructure.Helpers;
+global using bjorkvalle.Infrastructure.Utilities;
+global using System.Net.Http;
+global using System.Net.Http.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System;
+global using System.Text;
+global using System.Text.Json;
+global using System.Collections.Generic;
+global using System.Linq;
